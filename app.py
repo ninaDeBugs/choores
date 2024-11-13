247acc668fa1a686052654ef0c4479d234b91faf
ninaDeBugs <amour.twaining@gmail.com>
2024-11-13 15:59:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from home import home_page
 from chores import all_chores_page, chore_detail_page
 
 def main():
+    st.cache_data.clear() 
     if "page" not in st.session_state:
         st.session_state.page = "login"
     if "selected_chore" not in st.session_state:
