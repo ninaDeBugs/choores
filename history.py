7467b050a3cdb49c8ebada40c715d07b0ec13446
ninaDeBugs <amour.twaining@gmail.com>
2025-03-16 17:50:12 -0400

    Update history.py
    next is based on who hasn't done it in the longest time, 
    instead of the previous default order

--- history.py
+++ history.py
@@ -14,25 +14,42 @@ def mark_as_done(chore, member_name, todays_date):
 
 def calc_next(chore):
     history = chore.get('history', [])
-    dft_order = chore.get('members')
+    default_order = chore.get('members', [])
     
-    if not history: # then first member in default order is up next
-        ans = dft_order[0]
-    else:
-        # Count how many times each member has done the chore
-        member_counter = {m: 0 for m in dft_order}
-        for entry in history:
-            member = entry['member'] 
-            member_counter[member] += 1
+    # 0. New chores, next is first in default order
+    if not history and not next:
+        return default_order[0]
+    
+    # 1. Members in history, in order from latest participant
+    history_order = []
+    seen = set()
+    for entry in reversed(history):
+        if entry['member'] not in seen:
+            history_order.append(entry['member'])
+            seen.add(entry['member'])
+    
+    # 2. Count how many times each member has done the chore
+    member_count = {member: 0 for member in default_order}
+    for entry in history:
+        member_count[entry['member']] += 1
+    
+    # 3. Find the members with lowest count
+    min_count = min(member_count.values())
+    least_participative_members = [member for member, count in member_count.items() if count == min_count]
+    
+    # 4. If everyone has done it the same number of times, return the person who did it longest time ago
+    if len(least_participative_members) == len(default_order):
+        chore['history'] = []
+        return history_order[-1] 
+    
+    # 5. If everyone hasn't done it the same number of times
+        # 5a. Return someone who hasn't done it at all
+    for member in least_participative_members:
+        if member not in history_order:
+            least_participative_members.remove(member)
+            return member
 
-        member_counter_set = set(member_counter.values())
-        if len(member_counter_set) == 1:
-            # Reset the history if everyone's count is the same
-            chore['history'] = []
-            ans = dft_order[0]
-        else:
-            # Find the member with the lowest count; break ties using default order
-            leastnum = min(member_counter.values())
-            ans = next(m for m in dft_order if member_counter[m] == leastnum)
-
-    return ans
+        #5b. Return the person who's done it but did it the longest time ago
+    for member in reversed(history_order): 
+        if member in least_participative_members:
+            return member
